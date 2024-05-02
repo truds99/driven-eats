83cdba42b259b66199411fd7fd66bbe29d077fd1
truds99 <dudscardoso9@gmail.com>
2024-05-02 19:18:38 -0300

    driven flex version

--- scrypt.js
+++ scrypt.js
@@ -1,7 +1,10 @@
 let pratoSelecionado, bebidaSelecionada, sobreSelecionada;
-const msg = document.querySelector("button").innerHTML; // para voltar o texto inicial ao desmarcar uma opção
-const botao = document.querySelector("button"); // para alterar o background do botão
-
+const msg = document.querySelector("button").innerHTML;
+const botao = document.querySelector("button");
+const confirmacao = document.querySelector(".escondido");
+const pratoJanela = document.querySelector(".valor-Prato");
+const bebidaJanela = document.querySelector(".valor-Bebida");
+const sobreJanela = document.querySelector(".valor-Sobremesa");
 document.querySelector("button").disabled = true;
 
 function converterNumero (precoString) {
@@ -13,11 +16,8 @@ function converterNumero (precoString) {
 
 function selecaoPrato(elemento) {
     pratoSelecionado = document.querySelector(".prato.selecionado");
-    if (pratoSelecionado === elemento){}
-    else{
-        if (pratoSelecionado !== null) {
-            pratoSelecionado.classList.remove("selecionado");
-        }
+    if (!(pratoSelecionado === elemento) && (pratoSelecionado !== null)) {
+        pratoSelecionado.classList.remove("selecionado");
     }    
     elemento.classList.toggle("selecionado");
     pratoSelecionado = document.querySelector(".prato.selecionado");
@@ -26,11 +26,8 @@ function selecaoPrato(elemento) {
 
 function selecaoBebida(elemento) {
     bebidaSelecionada = document.querySelector(".bebida.selecionado");
-    if (bebidaSelecionada === elemento){}
-    else{
-        if (bebidaSelecionada !== null) {
-            bebidaSelecionada.classList.remove("selecionado");
-        }
+    if (!(bebidaSelecionada === elemento) && bebidaSelecionada !== null) {
+        bebidaSelecionada.classList.remove("selecionado");
     }  
     elemento.classList.toggle("selecionado");
     bebidaSelecionada = document.querySelector(".bebida.selecionado");
@@ -39,12 +36,9 @@ function selecaoBebida(elemento) {
 
 function selecaoSobre(elemento) {
     sobreSelecionada = document.querySelector(".sobremesa.selecionado");
-    if (sobreSelecionada === elemento){}
-    else{
-        if (sobreSelecionada !== null) {
-            sobreSelecionada.classList.remove("selecionado");
-        }
-    }  
+    if (!(sobreSelecionada === elemento) && sobreSelecionada !== null) {
+        sobreSelecionada.classList.remove("selecionado");
+    }    
     elemento.classList.toggle("selecionado");
     sobreSelecionada = document.querySelector(".sobremesa.selecionado");
     pedido();
@@ -63,22 +57,56 @@ function pedido() {
     }
 }   
 
-function whatsapp(){
-    let nome = prompt("Qual o seu nome?");
-    let endereco = prompt("Qual o seu endereço?");  
+function confirmarPedido(){
     let prato = pratoSelecionado.querySelector("h4").innerHTML;
     let bebida = bebidaSelecionada.querySelector("h4").innerHTML;
     let sobremesa = sobreSelecionada.querySelector("h4").innerHTML;
-    let precoTotal = converterNumero(pratoSelecionado.querySelector("h6").innerHTML) + converterNumero(bebidaSelecionada.querySelector("h6").innerHTML) + converterNumero(sobreSelecionada.querySelector("h6").innerHTML);
-    let msg = `Olá, gostaria de fazer o pedido: 
-    - Prato: ${prato}
-    - Bebida: ${bebida}
-    - Sobremesa: ${sobremesa}
-    Total: R$ ${precoTotal.toFixed(2)}
-    
-    Nome: ${nome}
-    Endereço: ${endereco}`;
+    let precoPrato = pratoSelecionado.querySelector("h6").innerHTML;
+    let precoBebida = bebidaSelecionada.querySelector("h6").innerHTML;
+    let precoSobremesa = sobreSelecionada.querySelector("h6").innerHTML;
+    let precoTotal =
+        converterNumero(precoPrato) +
+        converterNumero(precoBebida) +
+        converterNumero(precoSobremesa)
+    ;
+    pratoJanela.querySelector(".item").innerHTML = prato;
+    pratoJanela.querySelector(".preco").innerHTML = precoPrato;
+    bebidaJanela.querySelector(".item").innerHTML = bebida;
+    bebidaJanela.querySelector(".preco").innerHTML = precoBebida;
+    sobreJanela.querySelector(".item").innerHTML = sobremesa;
+    sobreJanela.querySelector(".preco").innerHTML = precoSobremesa;
+    confirmacao.querySelector(".valor-Total").lastElementChild.innerHTML = precoTotal.toFixed(2).replace('.', ',');
+    confirmacao.classList.add("confirmacao");
+    confirmacao.classList.remove("escondido");
+}
+
+function whatsapp(){
+    let prato = pratoSelecionado.querySelector("h4").innerHTML;
+    let bebida = bebidaSelecionada.querySelector("h4").innerHTML;
+    let sobremesa = sobreSelecionada.querySelector("h4").innerHTML;
+    let precoPrato = pratoSelecionado.querySelector("h6").innerHTML;
+    let precoBebida = bebidaSelecionada.querySelector("h6").innerHTML;
+    let precoSobremesa = sobreSelecionada.querySelector("h6").innerHTML;
+
+    let precoTotal =
+        converterNumero(precoPrato) +
+        converterNumero(precoBebida) +
+        converterNumero(precoSobremesa);
+
+    let msg = 
+        `Olá, gostaria de fazer o pedido: 
+        - Prato: ${prato}
+        - Bebida: ${bebida}
+        - Sobremesa: ${sobremesa}
+        Total: R$ ${precoTotal.toFixed(2).replace(".", ",")}`
+    ;
+
     msg = encodeURIComponent(msg);
-    const url = `https://wa.me/?text=${msg}`;
+    const url = `https://wa.me//5532984967075?text=${msg}`;
     window.open(url);
+}
+
+function fecharJanela() {
+    confirmacao.classList.add("escondido");
+    confirmacao.classList.remove("confirmacao");
 }
